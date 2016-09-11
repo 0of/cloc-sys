8eeb666de6fd5fa1b0eaa56a6a8d82742aab2da6
magnus <Magnusbackyard@live.com>
2016-09-11 14:11:37 +0800

    add cloc-webhook to dev group

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[lein-sub "0.3.0"]]
-  :profiles {:dev {:sub ["cloc-reporter" "cloc-scheduler" "cloc-web"]}
+  :profiles {:dev {:sub ["cloc-reporter" "cloc-scheduler" "cloc-web" "cloc-webhook"]}
              :migration {:sub ["cloc-migration"]}}
   :aliases {"test" ["with-profile" "dev" "sub" "test"] 
             "migrate" ["with-profile" "migration" "sub" "migrate"]})
