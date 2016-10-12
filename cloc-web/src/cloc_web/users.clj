db2a6ef6d84121db0ba63f2317768a2c0c6f1d16
magnus <Magnusbackyard@live.com>
2016-10-12 17:52:46 +0800

    display lang prop

--- cloc-web/src/cloc_web/users.clj
+++ cloc-web/src/cloc_web/users.clj
@@ -10,5 +10,18 @@
       (-> (r/table "users")
           (r/insert {:id id
                      :user user
-                     :filter "*"})
+                     :filter "*"
+                     :lang "SUM"})
+          (r/run conn)))))
+
+(defn update-display-lang
+  [{:keys [user repo]} body]
+  (let [id (format "github/%s/%s" user repo)
+        new-lang (if (> (count (:lang body)) 0)
+                    (:lang body)
+                    "SUM")]
+    (with-open [conn (r/connect :host "127.0.0.1" :port 28015 :db "cloc")]
+      (-> (r/table "users")
+          (r/get id)
+          (r/update {:lang new-lang})
           (r/run conn)))))
