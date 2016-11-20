35eeacaac27482796ae9c8dc6312c2ad419a8ea7
magnus <Magnusbackyard@live.com>
2016-11-20 19:03:48 +0800

    core

--- cloc-webapp/src/cloc_webapp/core.clj
+++ cloc-webapp/src/cloc_webapp/core.clj
@@ -1,7 +1,17 @@
 (ns cloc-webapp.core
+  (:require [ring.adapter.jetty :as jetty]
+            [compojure.core :refer :all]
+            [ring.middleware.params :as params]
+            [compojure.handler :as handler])
   (:gen-class))
 
+(defroutes app)
+  ;; main page
+
+(def handlers
+  (-> (handler/site app)
+      params/wrap-params))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (jetty/run-jetty handlers {:port 8080}))
