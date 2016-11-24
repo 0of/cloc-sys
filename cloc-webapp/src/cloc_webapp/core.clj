e42ff3820cab5c8403de87c12a9bd91480383dbc
magnus <Magnusbackyard@live.com>
2016-11-24 17:11:13 +0800

    main page

--- cloc-webapp/src/cloc_webapp/core.clj
+++ cloc-webapp/src/cloc_webapp/core.clj
@@ -1,6 +1,7 @@
 (ns cloc-webapp.core
   (:require [ring.adapter.jetty :as jetty]
             [compojure.core :refer :all]
+            [compojure.route :as route]
             [ring.util.response :refer [resource-response]])          
   (:gen-class))
 
@@ -8,8 +9,9 @@
   ;; main page
   (GET  "/" [] (resource-response "index.html" {:root "public"}))
   (GET  "/dash" [] (resource-response "dash.html" {:root "public"}))
-  (GET  "/login" [] (resource-response "login.html" {:root "public"})))  
+  (GET  "/login" [] (resource-response "login.html" {:root "public"}))
+  (route/resources "/"))
 
 (defn -main
   [& args]
-  (jetty/run-jetty app {:port 8080}))
+  (jetty/run-jetty app {:port 8000}))
