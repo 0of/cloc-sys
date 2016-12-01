93af1ba1507b8055a409074bc8597e4c300fff77
magnus <Magnusbackyard@live.com>
2016-12-01 21:09:40 +0800

    repo-list-view

--- cloc-webapp/src-cljs/dash/dash.cljs
+++ cloc-webapp/src-cljs/dash/dash.cljs
@@ -7,3 +7,20 @@
             [cljs-http.client :as http]))
 
 (def app-state (atom {}))
+
+;; left panel to list all the repos
+(defn repo-list-view [state owner]
+  (reify
+    om/IDidMount
+    (did-mount [this]    
+      (go (let [resp (<! (http/get "http://localhost:4679/users/repos" {:oauth-token (om/get-state this :auth)}))  
+                repos (:body resp)]
+            (when repos
+              (om/set-state! this :repos repo)))))      
+   
+    om/IRenderState
+    (render-state [this state]  
+      (if (:repos state)
+        (apply dom/ul nil
+          (map (fn [repo] (dom/li nil (:full_name repo))) (:repos this)))))))
+
