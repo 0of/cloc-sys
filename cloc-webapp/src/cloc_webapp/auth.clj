8e88e725f9bf835375a60f1aff4a46d239b308c1
magnus <Magnusbackyard@live.com>
2016-12-07 17:41:40 +0800

    fix assertions

--- cloc-webapp/src/cloc_webapp/auth.clj
+++ cloc-webapp/src/cloc_webapp/auth.clj
@@ -1,13 +1,12 @@
 (ns cloc-webapp.auth
   (:require [clj-http.client :as client]
-            [cheshire.core :refer [parse-string]]
-            [clojure.tools.logging :refer [info]]))
+            [cheshire.core :refer [parse-string]]))
 
 (defn auth
   [{api-server :api-server}]
-  (if-let [link (-> format "%s/github/auth" api-server) 
-                (client/get {:accept :json})
-                get-in [:body :link]]
+  (if-let [link (-> (format "%s/github/auth" api-server) 
+                    (client/get {:accept :json})
+                    (get-in [:body :link]))]
     {:status 302 :headers {"Location" link} :body ""}
     {:status 401 :body "access denied (authorization)"}))
 
@@ -15,7 +14,7 @@
   [{api-server :api-server params :params}]
   (if-let [token (-> (format "%s?%s" api-server (ring.util.codec/form-encode params))
                      (client/get {:accept :json})
-                     get-in [:body :token])]
+                     (get-in [:body :token]))]
     {:status 302 :headers {"Location" "/dash"} :body "" :cookies {"session_id" {:value token}}}
     {:status 401 :body "access denied (authorization)"}))
   
