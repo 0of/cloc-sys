8b693ade43a1ea3080d61c41a8d957cebca667da
magnus <Magnusbackyard@live.com>
2016-12-13 17:09:18 +0800

    render-dash & dash page

--- cloc-webapp/src-cljs/dash/dash.cljs
+++ cloc-webapp/src-cljs/dash/dash.cljs
@@ -1,30 +1,29 @@
 (ns cloc-webapp.dash
   (:require-macros [cljs.core.async.macros :refer [go alt!]])
-  (:require [goog.events :as events]
-            [cljs.core.async :refer [put! <! >! chan timeout]]
+  (:require [goog.json :as json]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs-http.client :as http]))
+            [cljs-http.client :as http])
+  (:import [goog.net Cookies]))  
 
 (def app-state (atom {}))
 
-;; left panel to list all the repos
 (defn repo-list-view [state owner]
   (reify
-    om/IDidMount
-    (did-mount [this]    
-      (go (let [resp (<! (http/get "/api/user/me" {:oauth-token (om/get-state this :auth)}))]  
-            (if (= 200 (:status resp))
-               (om/set-state! this :me (:body resp))
-               (om/set-state! this :repos repo)))))      
-   
     om/IRenderState
     (render-state [this state]  
-      (if (:repos state)
-        (apply dom/ul nil
-          (map (fn [repo] (dom/li nil (:full_name repo))) (:repos this)))))))
+      (apply dom/ul nil
+        (map (fn [repo] (dom/li nil (:full_name repo))) state)))))
 
+(defn widget [state owner]
+  (reify  
+    om/IInitState
+    (init-state [_]
+      {:me (json/parse (.-cloc_me js/window))
+       :auth (.get (Cookies. js/document) "session_id")})
 
-
+    om/IRenderState
+    (render-state [this state]
+      (build repo-list-view {:state (:repo state)}))))
 
 (om/root widget app-state {:target (.getElementById js/document "content")})
