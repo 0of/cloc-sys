7d76b80cfca9276489c91549ff3148590cf1ffbe
magnus <Magnusbackyard@live.com>
2017-01-10 17:09:16 +0800

    bootstrap main

--- cloc-webapp/src-cljs/main/core.cljs
+++ cloc-webapp/src-cljs/main/core.cljs
@@ -18,17 +18,21 @@
       {:auth (.get (Cookies. js/document) "session_id")})
 
     om/IRenderState
-    (render-state [this state]  
-      (if (:user state)
-        (dom/a #js {:href "/dash"} "Dashboard")
-        (dom/button
-            #js {:onClick #(open "https://github.com/login/oauth/authorize?client_id=")}   
-            "Github Login")))
+    (render-state [this state]
+      (dom/nav #js {:className "navbar navbar-default navbar-static-top"}
+        (dom/div #js {:className "container-fluid"}
+          (dom/a #js {:className "navbar-brand" :href "#"} "CLOC")
+
+          (dom/ul #js {:className "nav navbar-nav navbar-right"}
+            (if (:user state)
+              (dom/a #js {:href "/dash" :className "navbar-link"} "Dashboard")
+              (dom/a #js {:href "https://github.com/login/oauth/authorize?client_id=" :className "navbar-link"} "Github Login"))))))
 
     om/IDidMount
     (did-mount [this]
       (go (let [resp (<! (http/get "/api/user/is_login" {:oauth-token (om/get-state owner :auth)}))
                 user (get-in resp [:body :user])]
+            (prn resp)
             (when user
               (om/set-state! owner :user user)))))))
 
